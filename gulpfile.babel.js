3f799d63ab6b2572b7b88e5b43265bf2de905656
James Williams <james@jamesrwilliams.co.uk>
2016-09-30 10:48:42 +0100

    MailChimp tasks added to watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -113,8 +113,8 @@ function server(done) {
 
 // Watch for file changes
 function watch() {
-  gulp.watch('src/pages/**/*.html').on('change', gulp.series(pages, inline, browser.reload));
-  gulp.watch(['src/layouts/**/*', 'src/partials/**/*']).on('change', gulp.series(resetPages, pages, inline, browser.reload));
+  gulp.watch('src/pages/**/*.html').on('change', gulp.series(pages, inline, mailchimp, browser.reload));
+  gulp.watch(['src/layouts/**/*', 'src/partials/**/*']).on('change', gulp.series(resetPages, pages, inline, mailchimp, browser.reload));
   gulp.watch(['../scss/**/*.scss', 'src/assets/scss/**/*.scss']).on('change', gulp.series(resetPages, sass, pages, inline, mailchimp, browser.reload));
   gulp.watch('src/assets/img/**/*').on('change', gulp.series(images, browser.reload));
 }
